be1583dd667e14764b7ff0f66d80ec5d07c186f8
Yash Kamdar <yjkamdar@ncsu.edu>
2019-11-16 22:16:31 -0500

    management dhcp setup

---
+++ scripts/create_veth_ns_br.sh
@@ -0,0 +1,23 @@
+##./create_veth_ns1_ns2 ns ns_ip br e1 e2
+
+#ns
+ns=$1
+ns_ip=$2
+
+#bridge
+br=$3
+
+#veth pair ids
+e1=$4
+e2=$5
+
+ip link add $e1 type veth peer name $e2
+ip link set $e1 netns $ns1
+brctl addif $br $e2
+
+#configure ip of ns
+ip netns exec $ns ip addr add $ns_ip dev $e1
+ip netns exec $ns ip link set $e1 up
+
+#configure bridge if
+ip link set $e2 up
