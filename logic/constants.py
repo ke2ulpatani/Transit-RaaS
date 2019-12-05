46f2eb77af1cd21684a32eedb98860a9679f4daa
ke2ulpatani <ke2ul.patani@gmail.com>
2019-12-05 11:26:08 +0000

    create_vpc logic

---
+++ logic/constants.py
@@ -0,0 +1,29 @@
+"""
+Identifiers:
+hypervisor: h
+customer: c
+vpc: v
+spine: s
+l1transit: t1
+l2transit: t2
+leaf: l
+vm: vm
+namespace: ns
+veth: ve_d1_d2 (d1->d2)
+bridge: b
+network: net
+subnet: nid
+manamgement: m
+management namespace: nsm
+"""
+
+metadata_file="../var/metadata.json"
+cid_file="../var/cid"
+mgmt_net_file = "../etc/networks/mgmt_net.json"
+hypervisors_file = "../misc/hypervisors.json"
+brid_file="../var/brid"
+h1_nsm_ip_file="../var/h1_nsm_ip"
+nsm_h1_ip_file="../var/nsm_h1_ip"
+h2_nsm_ip_file="../var/h2_nsm_ip"
+nsm_h2_ip_file="../var/nsm_h2_ip"
+
---
+++ logic/create_vpc.py
@@ -0,0 +1,56 @@
+import sys
+#from do_json import *
+import do_json
+import raas_utils
+import constants
+import ipaddress
+
+"""@params:
+    param1 = vpc config file (required)
+"""
+
+if __name__=="__main__":
+    if (len(sys.argv) < 2):
+        print("Please give vpc config file")
+        exit(1)
+
+    vpc_config_file = sys.argv[1]
+
+    #Assumed customer always gives correct config file
+    vpc_data = do_json.json_read(vpc_config_file)
+
+    hypervisor = vpc_data["hypervisor_name"]
+
+    hypervisors_data = do_json.json_read(hypervisors_file)
+
+    if hypervisor not in hypervisors_data:
+        print("unknown hypervisor")
+        exit(1)
+
+    hypervisor_ip = hypervisors_data[hypervisor]
+
+    if not raas_utils.exists_mgmt_ns():
+        cid = raas_utils.get_client_id()
+
+        nsm = "nsm=c" + cid + "_" + "nsm"
+        b_net = "b_net=c" + cid + "_m_net"
+        b = "b=c" + cid + "_m_b"
+        nid = "nid=" + raas_utils.get_mgmt_nid()
+        ve_h_nsm = "ve_h_nsm=c" + cid + "ve_h_nsm"
+        ve_nsm_h = "ve_nsm_h=c" + cid + "ve_nsm_h"
+        ve_nsm_b = "ve_nsm_b=c" + cid + "ve_nsm_b"
+        ve_b_nsm = "ve_b_nsm=c" + cid + "ve_b_nsm"
+        h_nsm_ip = "h_nsm_ip=" + raas_utils.get_h_nsm_ip(hypervisor)
+        nsm_h_ip = "nsm_h_ip" + raas_utils.get_nsm_h_ip(hypervisor)
+
+        subnet = nid.split('/')
+        b_ip = "b_ip=" + str(ipaddress.ip_address(subnet[0])+1) + '/' + subnet[1]
+        dhcp_range = "dhcp_range=" + str(ipaddress.ip_address(subnet[0])+2)+','+ \
+                str(ipaddress.ip_address(subnet[0])+254)
+
+        extra_vars = nsm + " " + b_net + " " + b + " " + \
+                nid + " " + ve_h_nsm + " " + ve_nsm_h + " " + \
+                ve_nsm_b + " " + ve_b_nsm + " " + h_nsm_ip + \
+                " " + nsm_h_ip + " " + b_ip + " " + dhcp_range
+
+        print("ansible-playbook -i hosts.yml --extra") 
