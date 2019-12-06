b0eabd588c2a505fc18508926750cd9b082ac0c6
ke2ulpatani <ke2ul.patani@gmail.com>
2019-12-06 01:09:08 -0500

    anupam mistake

--- logic/raas_utils.py
+++ logic/raas_utils.py
@@ -8,8 +8,8 @@ def get_mgmt_nid():
     return json_data["network_id"]
 
 def client_exists_vpc(vpc_name):
-    return os.path.exists(var_vpc + vpc_name)
+    return os.path.exists(constants.var_vpc + vpc_name)
 
 def client_add_vpc(vpc_name):
-    with open(var_vpc + vpc_name, "w") as f:
+    with open(constants.var_vpc + vpc_name, "w") as f:
         f.write(vpc_name)
