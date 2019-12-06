9ef758a5d14bfaa1de89dfc083f75b782c140952
ke2ulpatani <ke2ul.patani@gmail.com>
2019-12-06 22:10:45 +0000

    create_spine decoupled

--- logic/create_spine.py
+++ logic/create_spine.py
@@ -61,47 +61,88 @@ if __name__=="__main__":
     cid = hyp_utils.get_client_id()
 
     try:
-        sid = hyp_utils.get_spine_id(hypervisor, vpc_name)
-        image_arg = "image_path="+constants.img_path + \
-                constants.spine_vm_img
-        spine_name_ansible = "c" + str(cid) + "_" + "s" + str(sid)
-        spine_name_ansible_arg = "s_name="+spine_name_ansible
-        c_s_image_path_arg = "c_s_image_path="+constants.img_path+ \
-                spine_name_ansible + ".img"
-
-        s_ram_arg = "s_ram=" + str(mem)
-        s_vcpu_arg = "s_vcpu=" + str(vcpu)
-
-        mgt_net_arg = "mgt_net=" + hyp_utils.get_mgmt_net(cid)
-
-        extra_vars = constants.ansible_become_pass + " " + \
-                image_arg + " " +  \
-                s_ram_arg + " " + s_vcpu_arg + " " + \
-                mgt_net_arg + " " + spine_name_ansible_arg + \
-                " " + c_s_image_path_arg + " " +  hypervisor_arg
-
-        #print("here2")
+        #create_spine
         try:
+            sid = hyp_utils.get_spine_id(hypervisor, vpc_name)
+            image_arg = "image_path="+constants.img_path + \
+                    constants.spine_vm_img
+            spine_name_ansible = "c" + str(cid) + "_" + "s" + str(sid)
+            spine_name_ansible_arg = "s_name="+spine_name_ansible
+            c_s_image_path_arg = "c_s_image_path="+constants.img_path+ \
+                    spine_name_ansible + ".img"
+
+            s_ram_arg = "s_ram=" + str(mem)
+            s_vcpu_arg = "s_vcpu=" + str(vcpu)
+
+            mgt_net_arg = "mgt_net=" + hyp_utils.get_mgmt_net(cid)
+
+            extra_vars = constants.ansible_become_pass + " " + \
+                    image_arg + " " +  \
+                    s_ram_arg + " " + s_vcpu_arg + " " + \
+                    mgt_net_arg + " " + spine_name_ansible_arg + \
+                    " " + c_s_image_path_arg + " " +  hypervisor_arg
+
+            #print("here2")
             print("ansible-playbook logic/vpc/create_spine.yml -i logic/inventory/hosts.yml -v --extra-vars '"+extra_vars+"'")
             rc = os.system("ansible-playbook logic/vpc/create_spine.yml -i logic/inventory/hosts.yml -v --extra-vars '"+extra_vars+"'")
             if (rc != 0):
                 raise
+                
+            hyp_utils.write_spine_id(sid+1, vpc_name, hypervisor)
+            #print("here4", spine_name, vpc_name, hypervisor, spine_name_ansible)
+            hyp_utils.vpc_add_spine(hypervisor, vpc_name, spine_name, spine_name_ansible)
+            raas_utils.client_add_spine(vpc_name, spine_name)
+
             #raise
             #raas_utils.add_mgmt_ns(hypervisor)
         except:
-            #os.system("ansible-playbook logic/vpc/delete_spine.yml -i logic/inventory/hosts.yml -v --extra-vars '"+extra_vars+"'")
+            print("create spine failed")
             print("ansible-playbook logic/vpc/delete_spine.yml -i logic/inventory/hosts.yml -v --extra-vars '"+extra_vars+"'")
+            #os.system("ansible-playbook logic/vpc/delete_spine.yml -i logic/inventory/hosts.yml -v --extra-vars '"+extra_vars+"'")
+            raise
+        #print("ansible-playbook logic/vpc/delete_spine.yml -i logic/inventory/hosts.yml -v --extra-vars '"+extra_vars+"'")
 
         #print("here3", sid+1, vpc_name, hypervisor)
 
-        hyp_utils.write_spine_id(sid+1, vpc_name, hypervisor)
 
-        #print("here4", spine_name, vpc_name, hypervisor, spine_name_ansible)
-        hyp_utils.vpc_add_spine(hypervisor, vpc_name, spine_name, spine_name_ansible)
+        #get spine ip and store to some file
+        try:
+            vm_name_arg = "vm_name="+spine_name_ansible
+            ip_file_path_arg = "ip_path="+temp_file
+            extra_vars = constants.ansible_become_pass + " " + \
+                    " " + vm_name_arg + " " +  hypervisor_arg + ip_file_path_arg
 
-        #print("here5", spine_name, vpc_name)
+            print("ansible-playbook logic/misc/get_vm_ip.yml -i logic/inventory/hosts.yml -v --extra-vars '"+extra_vars+"'")
+            rc = os.system("ansible-playbook logic/misc/get_vm_ip.yml -i logic/inventory/hosts.yml -v --extra-vars '"+extra_vars+"'")
+            if (rc != 0):
+                raise
 
-        raas_utils.client_add_spine(vpc_name, spine_name)
+            spine_ip = raas_utils.read_temp_file()
+            raas_utils.write_spine_ip(vpc_name, spine_name, spine_ip)
+        except:
+            print ("get_vm_ip playbook failed")
+            raise
 
+        #instal quagga
+        try:
+            spine_ip = raas_utils.get_spine_ip(vpc_name, spine_name)
+            spine_ip_arg = "vm_ip="+spine_ip
+
+            ######
+            extra_vars = constants.ansible_become_pass + " " + \
+                    " " + constants.ansible_ssh_private_key + " " + spine_ip_arg
+
+            --ssh-common-args='-o ProxyCommand="ssh -i ~/.ssh/id_raas ece792@172.16.2.1 -W %h:%p"'
+            ssh_common_args = "-o ProxyCommand=\"ssh -i" + constants.ansible_ssh_private_key + " ece792@" + hypervisor_ip + " " +\
+                    "-W %h:%p\""
+
+            print("ansible-playbook logic/misc/quagga_install.yml -i \""+spine_ip+",\" -v --extra-vars '"+extra_vars+"'"\
+                    + " --ssh_common_args='"+ssh_common_args+"'")
+            #rc = os.system("ansible-playbook logic/misc/quagga_install.yml -i \""+spine_ip+",\" -v --extra-vars '"+extra_vars+"'"\
+            #        + " --ssh_common_args='"+ssh_common_args+"'")
+            #if (rc != 0):
+            #    raise
+        except:
+            print("quagga_install playbook failed")
     except:
-        print("create spine failed")
+        print("create spine failed python failed")
--- logic/raas_utils.py
+++ logic/raas_utils.py
@@ -8,7 +8,7 @@ def get_mgmt_nid():
     return json_data["network_id"]
 
 def client_exists_vpc(vpc_name):
-    return os.path.exists(constants.var_vpc + vpc_name + "/" + vpc_name)
+    return os.path.exists(constants.var_vpc + vpc_name + "/" + vpc_name + ".json")
 
 def client_add_vpc(vpc_name):
     vpc_dir = constants.var_vpc + vpc_name + "/"
@@ -16,8 +16,8 @@ def client_add_vpc(vpc_name):
     if not os.path.exists(vpc_dir):
         os.makedirs(vpc_dir)
 
-    with open(vpc_dir + vpc_name, "w") as f:
-        f.write(vpc_name)
+    new_vpc_data = {"name": vpc_name}
+    do_json.json_write(new_vpc_data, f)
 
     vpc_spines_dir = vpc_dir + constants.vpc_spines
     if not os.path.exists(vpc_spines_dir):
@@ -27,38 +27,66 @@ def client_add_vpc(vpc_name):
     if not os.path.exists(vpc_leafs_dir):
         os.makedirs(vpc_leafs_dir)
 
-    vpc_bridges_dir = vpc_dir + constants.vpc_bridges
-    if not os.path.exists(vpc_bridges_dir):
-        os.makedirs(vpc_bridges_dir)
-
     vpc_pcs_dir = vpc_dir + constants.vpc_pcs
     if not os.path.exists(vpc_pcs_dir):
         os.makedirs(vpc_pcs_dir)
 
 def client_exists_spine(vpc_name, spine_name):
     file_path = constants.var_vpc + vpc_name + \
-            constants.vpc_spines + spine_name
+            constants.vpc_spines + spine_name+ ".json"
 
     return os.path.exists(file_path)
 
 def client_add_spine(vpc_name, spine_name):
     file_path = constants.var_vpc + vpc_name + \
-            constants.vpc_spines + spine_name
-    with open(file_path, "w") as f:
-        f.write(spine_name)
+            constants.vpc_spines + spine_name + ".json"
 
-def client_exists_pc(vpc_name, pc_name):
+    new_spine_data = {"name": spine_name}
+    do_json.json_write(new_spine_data, f)
+
+def client_exists_leaf(vpc_name, leaf_name):
     file_path = constants.var_vpc + vpc_name + \
-            constants.vpc_pcs + pc_name
+            constants.vpc_leafs + leaf_name + ".json"
 
     return os.path.exists(file_path)
 
+def client_add_leaf(vpc_name, leaf_name):
+    file_path = constants.var_vpc + vpc_name + \
+            constants.vpc_leafs + leaf_name + ".json"
+    new_leaf_data = {"name": leaf_name}
+    do_json.json_write(new_leaf_data, f)
+
+def client_exists_pc(vpc_name, pc_name):
+    file_path = constants.var_vpc + vpc_name + \
+            constants.vpc_pcs + pc_name + ".json"
+
+    return os.path.exists(file_path)
+
+
 def client_add_pc(vpc_name, pc_name):
     file_path = constants.var_vpc + vpc_name + \
-            constants.vpc_pcs + pc_name
-    with open(file_path, "w") as f:
-        f.write(pc_name)
+            constants.vpc_pcs + pc_name + ".json"
+    new_pc_data = {"name": pc_name}
+    do_json.json_write(new_pc_data, f)
 
+def write_spine_ip(vpc, spine, spine_ip):
+    file_path = constants.var_vpc + vpc_name + \
+            constants.vpc_spines + spine_name + ".json"
+
+    spine_data = do_json.json_read(file_path)
+    spine_data["ip"] = spine_ip
+    do_json.json_write(spine_data, file_path)
+
+def get_spine_ip(vpc, spine):
+    spine_data = do_json.json_read(file_path)
+    spine_ip = spine_data["ip"]
+    return spine_ip
+
+def read_temp_file():
+    with open(constants.temp_file) as f:
+        data = f.read()
+
+    return data
 #def client_exists_bridge(vpc_name, bridge_name):
 #    file_path = constants.var_vpc + vpc_name + \
 #            constants.vpc_bridges + bridge_name
