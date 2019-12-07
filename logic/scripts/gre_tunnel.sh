b5d86d415b6219f5143fce0ad6eadfdad7f4b126
ke2ulpatani <ke2ul.patani@gmail.com>
2019-12-07 18:33:36 -0500

    fixed gre tunnnel

--- logic/scripts/gre_tunnel.sh
+++ logic/scripts/gre_tunnel.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#./gre_tunnel_vm add gretunnelname local_ip remote_ip remote_subnet
+
 #Default mode "gre"
 mode="gre"
 
@@ -11,25 +13,16 @@ name=$2
 
 #Name of namespaces as endpoints and corresponding ip addresses with VM subnets
 #Refer readme for topology
-ns1=$3
-ns1_ip=$4
-ns1_vm_subnet=$5
 
-ns2=$6
-ns2_ip=$7
-ns2_vm_subnet=$8
+local_ip=$3
+remote_ip=$4
+remote_subnet=$5
+
 
 if [ $action = "add" ]; then
 	#at ns1
-	sudo ip netns exec $ns1 ip tunnel add $name mode $mode local $ns1_ip remote $ns2_ip
-	sudo ip netns exec $ns1 ip link set dev $name up
-	sudo ip netns exec $ns1 ip route add $ns2_vm_subnet dev $name
-
-	#at ns2
-	sudo ip netns exec $ns2 ip tunnel add $name mode $mode local $ns2_ip remote $ns1_ip
-	sudo ip netns exec $ns2 ip link set dev $name up
-	sudo ip netns exec $ns2 ip route add $ns1_vm_subnet dev $name
+	sudo ip tunnel add $name mode $mode local $local_ip remote $remote_ip
+	sudo ip link set dev $name up
 elif [ $action = "del" ]; then
-	sudo ip netns exec $ns1 ip tunnel del $name mode $mode local $ns1_ip remote $ns2_ip
-	sudo ip netns exec $ns2 ip tunnel del $name mode $mode local $ns2_ip remote $ns1_ip
+	sudo ip tunnel del $name mode $mode local $local_ip remote $remote_ip
 fi
