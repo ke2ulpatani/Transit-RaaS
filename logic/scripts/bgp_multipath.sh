61a132cda1651d2878e002a3c23707bf1f5dc4de
ke2ulpatani <ke2ul.patani@gmail.com>
2019-12-09 02:36:41 -0500

    added bgp default

--- logic/scripts/bgp_multipath.sh
+++ logic/scripts/bgp_multipath.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 self_as=$1
-c_name=$2
+activate=$2
+c_name=$3
 
 #setsebool zebra_write_config 1
 
-docker exec -it $c_name vtysh -c "conf t" -c "no router bgp 7675" -c "router bgp $self_as" \
+if [ $activate == "true" ]
+then
+    docker exec -it $c_name vtysh -c "conf t" -c "no router bgp 7675" -c "router bgp $self_as" \
 	-c "maximum-paths 8" \
 	-c "end" -c "write"
-
-
+else
+    docker exec -it $c_name vtysh -c "conf t" -c "no router bgp 7675" -c "router bgp $self_as" \
+	-c "maximum-paths 1" \
+	-c "end" -c "write"
+fi
